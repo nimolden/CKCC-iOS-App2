bdbe54a771c27d4954118668198f7b350de0f8c5
leapkh <leapkh@yahoo.com>
2018-11-05 19:19:58 +0700

    Initial Commit

---
+++ ckcc/EventTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  EventTableViewCell.swift
+//  ckcc
+//
+//  Created by Bun Leap on 11/5/18.
+//  Copyright © 2018 Cambodia-Korea Cooperation Center. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var eventImageView: UIImageView!
+    
+}
